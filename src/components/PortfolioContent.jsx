65733f3d705f209aa8c7abb9c74228e8675febb0
Nick Wong <fullycharged100@gmail.com>
2020-04-06 22:14:38 -0700

    link to github on portfolio

--- src/components/PortfolioContent.jsx
+++ src/components/PortfolioContent.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import ExternalLinkButton from './ExternalLinkButton';
 
 const PortfolioContent = (props) => {
     return (
         <div className="PortfolioContent Basic-Content">
             <h2>Currently under construction. Stay tuned!</h2>
+            <p>For now, you can find my projects on Github.</p>
+            <ExternalLinkButton label="Github" destination="https://github.com/tginick" />
+
         </div>
     );
 }
